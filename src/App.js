2c58b5460d360a2ce1951cc209d1ac503526cdf6
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2022-02-10 22:17:27 -0800

    Cleanup of file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Login from './Login.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
