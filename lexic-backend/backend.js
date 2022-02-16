e59264431f29f340c3c7a83aa48ebc8837f7c450
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2022-02-16 13:46:46 -0800

    Changing Port in backend.js
    Fixes issue with airplay conflicting port

--- lexic-backend/backend.js
+++ lexic-backend/backend.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const userServices = require("./models/user-services");
 
 const app = express();
-const port = 5000;
+const port = 1000;
 
 app.use(cors());
 app.use(express.json());
