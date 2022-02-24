b3a15de7f568fd0390ff98d8987d1abf6b395bc2
Tyler <tylertycahart@gmail.com>
2022-02-24 15:44:49 -0800

    uh oh, trying to fix start bug

--- lexic-backend/backend.js
+++ lexic-backend/backend.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 // Add mongdb user services
-const userServices = require("./models/user-services.js");
+const userServices = require("./models/user-services");
 
 const app = express();
 const port = 1000;
