7bd65f29947ccfc502e81fd2991b9697eae377bb
Tyler <tylertycahart@gmail.com>
2022-02-24 15:59:02 -0800

    added console.log for debug

--- lexic-backend/models/user-services.js
+++ lexic-backend/models/user-services.js
@@ -29,6 +29,7 @@ async function getUsers(name, job) {
   let result;
   if (name === undefined && job === undefined) {
     result = await userModel.find();
+    console.log("hello");
     console.log(result);
   } else if (name && !job) {
     result = await findUserByName(name);
