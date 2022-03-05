6b7be8729cfb764d81f5276bd7aba3ffe19c02c4
WJKw0k <snappytheturtle101@gmail.com>
2022-03-04 17:03:01 -0800

    Setup Routing with Login and Create Account

--- lexic-frontend/src/index.js
+++ lexic-frontend/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './Login';
+import CreateAccount from './CreateAccount';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path="create-account" element={<CreateAccount />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
